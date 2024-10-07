c27c7e6971199e25a868624b7fb2738c558f59f5
Raisul Islam <mdraju3248@gmail.com>
2024-10-08 00:38:21 +0600

    Add register form header

---
+++ register.php
@@ -0,0 +1,16 @@
+<?php include 'inc/header.php'; ?>
+<!-- register form design start -->
+<div>
+    <div class="d-flex justify-content-center align-items-center vh-80 bg-light">
+        <div class="col-md-4">
+            <div class="card shadow-lg border-0 rounded-lg">
+                <div class="card-header bg-primary text-white text-center py-4">
+                    <h3 class="mb-0">Register</h3>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+<!-- register form design end -->
+<?php include 'inc/footer.php'; ?>
