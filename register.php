ba84d20dd5dc1d2b67495cf831163e96eb19917a
Raisul Islam <mdraju3248@gmail.com>
2024-10-08 00:38:59 +0600

    Added register form layout design

--- register.php
+++ register.php
@@ -7,6 +7,29 @@
                 <div class="card-header bg-primary text-white text-center py-4">
                     <h3 class="mb-0">Register</h3>
                 </div>
+                <div class="card-body p-4">
+                    <form action="register.php" method="POST">
+                        <div class="form-group mb-3">
+                            <label for="name" class="form-label">Name</label>
+                            <input type="text" name="name" id="name" class="form-control" placeholder="Enter your name">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="username" class="form-label">Username</label>
+                            <input type="text" name="username" id="username" class="form-control" placeholder="Enter your username">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="email" name="email" id="email" class="form-control" placeholder="Enter your email">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="password" class="form-label">Password</label>
+                            <input type="password" name="password" id="password" class="form-control" placeholder="Enter your password">
+                        </div>
+                        <div class="form-group d-grid gap-2">
+                            <button type="submit" name="register" class="btn btn-success btn-block">Register</button>
+                        </div>
+                    </form>
+                </div>
 
             </div>
         </div>
