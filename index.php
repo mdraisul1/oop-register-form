ff4a9a47e506aefb2694fb40c986a41a1cf5592b
Raisul Islam <mdraju3248@gmail.com>
2024-10-08 00:04:32 +0600

    Add footer section

--- index.php
+++ index.php
@@ -39,7 +39,18 @@
         <!-- body content -->
 
 
-    
+        <!-- Remove the container if you want to extend the Footer to full width. -->
+        <div class="container my-5">
+            <footer class="text-center text-lg-start" style="background-color: #db6930;">
+                <!-- Copyright -->
+                <div class="text-center text-white p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+                    © 2024 Copyright:
+                    <a class="text-white" href="#">Login Register System</a>
+                </div>
+                <!-- Copyright -->
+            </footer>   
+        </div>
+        <!-- End of .container -->
 
     </div>
 </body>
