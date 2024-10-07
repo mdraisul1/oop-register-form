302d249946a8a74b627833e4f641757087e1115b
Raisul Islam <mdraju3248@gmail.com>
2024-10-08 00:31:11 +0600

    Added login form layout

--- login.php
+++ login.php
@@ -6,7 +6,22 @@
             <div class="card-header bg-primary text-white text-center py-4">
                 <h3 class="mb-0">Login</h3>
             </div>
-
+            <div class="card-body p-4">
+                <form action="login.php" method="POST">
+                    <div class="form-group mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" name="email" id="email" class="form-control" placeholder="Enter your email">
+                    </div>
+                    <div class="form-group mb-4">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" name="password" id="password" class="form-control" placeholder="Enter your password">
+                    </div>
+                    <div class="form-group d-grid gap-2">
+                        <button type="submit" name="login" class="btn btn-success btn-block">Login</button>
+                    </div>
+                </form>
+            </div>
+            
         </div>
     </div>
 </div>
